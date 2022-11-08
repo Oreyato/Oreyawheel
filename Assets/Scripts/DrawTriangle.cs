42dd65197c5e66a543acaec489de1bdb6867c5da
Oreyato <oreyato.pro@gmail.com>
2022-11-09 00:04:17 +0100

    initial commit

---
+++ Assets/Scripts/DrawTriangle.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+
+public class DrawTriangle : MonoBehaviour {
+
+    Mesh m;
+    MeshFilter mf;
+
+    private Vector3 originPos;
+    private Vector3 startingPos;
+    [SerializeField] private Vector3 endingPos;
+    [SerializeField] private Color color;
+
+    // Use this for initialization
+    void Start ()
+    {
+        mf = GetComponent<MeshFilter>();
+        m = new Mesh();
+        mf.mesh = m;
+        
+        startingPos = new Vector3(-3, 0, 0);
+    }
+
+    public void UpdateTriangle(Vector3 endingPosP, Color colorP)
+    {
+        endingPos = endingPosP;
+        color = colorP;
+        
+        Draw();
+        
+        // Change material color
+        gameObject.GetComponent<Renderer>().material.color = color;
+    }
+    
+    //This draws a triangle
+    void Draw()
+    {
+        //We need two arrays one to hold the vertices and one to hold the triangles
+        Vector3[] VerticesArray = new Vector3[3];
+        int[] trianglesArray = new int[3];
+
+        //lets add 3 vertices in the 3d space
+        VerticesArray[0] = originPos;
+        VerticesArray[1] = endingPos;
+        VerticesArray[2] = startingPos;
+
+        //define the order in which the vertices in the VerticesArray should be used to draw the triangle
+        trianglesArray[0] = 0;
+        trianglesArray[1] = 1;
+        trianglesArray[2] = 2;
+
+        //add these two triangles to the mesh
+        m.vertices = VerticesArray;
+        m.triangles = trianglesArray;
+    }
+}
---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,136 @@
+﻿using System.IO;
+using System.Net;
+using UnityEngine;
+
+[System.Serializable]
+public enum WheelOwner
+{
+    General,
+    Paul
+}
+[System.Serializable]
+public struct AllWheels
+{
+    public WheelItems[] allWheels;
+}
+[System.Serializable]
+public struct WheelItems
+{
+    public WheelOwner wheelOwner;
+    public WheelItem[] wheelItems;
+}
+
+[System.Serializable]
+public struct WheelItem {
+    public string itemName;
+    [Range(0.0f, 100.0f)]
+    public float itemPercentage;
+
+    public Color itemColor;
+}
+
+public class Wheel : MonoBehaviour
+{
+    [SerializeField] private GameObject markPrefab;
+    [SerializeField] private GameObject trianglePrefab;
+
+    [SerializeField] private WheelOwner wheelOwner;
+    
+    private WheelItem[] wheelItems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RetrieveData();
+        CreateWheel();
+    }
+
+    void CreateWheel()
+    {
+        float startingAngle = 0f;
+        
+        foreach (var item in wheelItems)
+        { 
+            GameObject spawnedMark = Instantiate(markPrefab, 
+                new Vector3(0.0f, -0.01f, 0.0f), 
+                new Quaternion(.0f, -180.0f, 0.0f, 0.0f));
+
+            GameObject spawnedTriangle = Instantiate(trianglePrefab,
+                new Vector3(0.0f, -0.01f, 0.0f), 
+                new Quaternion(.0f, .0f, 0.0f, 0.0f));
+
+            DrawTriangle triangleScript = spawnedTriangle.GetComponent<DrawTriangle>();
+
+            triangleScript.UpdateTriangle(
+                new Vector3(-3.0f, 0.5f, .0f), 
+                item.itemColor);
+
+            float itemAngle = PercentageToDegreeAngle(item.itemPercentage);
+            spawnedMark.GetComponent<Transform>().Rotate(Vector3.forward, startingAngle + itemAngle);
+            GameObject spawnedMarkChild = spawnedMark.transform.GetChild(0).gameObject;
+            spawnedMarkChild.GetComponent<Renderer>().material.color = item.itemColor;
+            spawnedMarkChild = spawnedMark.transform.GetChild(1).gameObject;
+            spawnedMarkChild.GetComponent<Renderer>().material.color = item.itemColor;
+
+            startingAngle += itemAngle;
+        }
+    }
+
+    float PercentageToDegreeAngle(float percentage)
+    {
+        return percentage * 360f / 100f;
+    }
+    
+    void LaunchWheel()
+    {
+        
+    }
+
+    void ReturnWheelResult()
+    {
+        
+    }
+
+    void SaveData()
+    {
+        // Guys wheels
+        WheelItems generalWheel = new WheelItems
+        {
+            wheelOwner = WheelOwner.General,
+            wheelItems = wheelItems
+        };
+        // Own wheel
+        WheelItems paulWheel = new WheelItems
+        {
+            wheelOwner = WheelOwner.Paul,
+            wheelItems = wheelItems
+        };
+
+        WheelItems[] allWheels = { generalWheel, paulWheel };
+        
+        AllWheels wheels = new AllWheels
+        {
+            allWheels = allWheels
+        };
+
+        string json = JsonUtility.ToJson(wheels);
+        
+        File.WriteAllText("S:/UnityProjects/PERSONAL_PROJECTS/Roue/Assets/Scripts/wheelsPatterns.json", json);
+    }
+    
+    void RetrieveData()
+    {
+        string jsonPath = File.ReadAllText("S:/UnityProjects/PERSONAL_PROJECTS/Roue/Assets/Scripts/wheelsPatterns.json");
+        
+        AllWheels allWheels = JsonUtility.FromJson<AllWheels>(jsonPath);
+        WheelItems[] wheels = allWheels.allWheels;
+
+        foreach (var wheel in wheels)
+        {
+            if (wheel.wheelOwner == wheelOwner)
+            {
+                wheelItems = wheel.wheelItems;
+            }
+        }
+    }
+}
